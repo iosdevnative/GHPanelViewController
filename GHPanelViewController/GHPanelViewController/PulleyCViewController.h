d4bd8946511d101981b165cdb612588cedfed733
Ryan Linn <guthookhikes@gmail.com>
2018-12-14 10:41:22 -0500

    Made setDrawerPosition:animated:completion: public

--- GHPanelViewController/GHPanelViewController/PulleyCViewController.h
+++ GHPanelViewController/GHPanelViewController/PulleyCViewController.h
@@ -194,6 +194,7 @@ typedef struct DrawerDistanceFromBottom {
 - (void) bounceDrawer:(CGFloat) height speedMultiplier:(CGFloat) multimplier;
 
 - (void) setDrawerPosition:(PulleyCPosition *) position animated:(BOOL) animated;
+- (void) setDrawerPosition:(PulleyCPosition *) position animated:(BOOL) animated completion:(PulleyAnimationCompletionBlock) completion;
 
 - (void) setPrimaryContentViewController:(UIViewController *) controller animated:(BOOL) animated completion:(_Nullable  PulleyAnimationCompletionBlock) completion;
 
